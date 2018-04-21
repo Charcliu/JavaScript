eb469450ec726d9331260735455632b777769aaa
Fluently <f_liuchang@163.com>
2018-04-21 17:56:11 +0800

    Learn JavaScript

--- 高级技巧.js
+++ 高级技巧.js
@@ -28,3 +28,44 @@
 			return new person(name, age, sex);
 		}
 	}
+
+不可扩展对象  Object.preventExtensions()
+密封的对象	 Object.seal()
+冻结的对象    Object.freeze()
+
+setTimeout()的链式调用
+ 
+setTimeout(function(){
+	console.log(new Date())
+	setTimeout(arguments.callee, 1000);
+},1000);
+
+Yield Processes
+
+setTimeout(function(){
+	var item = array.shift();
+	process(item);
+	if(array.length > 0){
+		setTimeout(arguments.callee, 1000)
+	}
+},1000)
+
+函数节流
+
+var processor = {
+	timeoutId : null,
+	performProcessing : function(){
+		console.log(new Date())
+	},
+	process : function(){
+		clearTimeout(this.timeoutId);
+		var that = this;
+		this.timeoutId = setTimeout(function(){
+			that.performProcessing();
+		},1000)
+	}
+}
+
+for(let i = 0; i < 1000; i++){
+	processor.process();
+}
